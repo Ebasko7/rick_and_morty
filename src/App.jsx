1e925f5a89848816594034fc6b3ad27640993a86
ERIC_BASKOVICH <eric.baskovich@gmail.com>
2024-07-01 11:20:35 -0600

    git init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+import MyNavBar from "./components/Navbar"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <MyNavBar />
+    <Outlet />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,8 @@
+function AboutPage (){
+
+    return(
+        <h1>ABOUT</h1>
+    )
+}
+
+    export default AboutPage
